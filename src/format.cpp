006212b9c5426cc26fce61865baaba01519d7ee1
edghyhdz <edgar.hdz@hotmail.com>
2020-11-11 21:10:51 +0100

    Completed ElapsedTime function

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,27 @@
+#include "format.h"
+#include <iomanip>
+#include <iostream>
 #include <string>
 
-#include "format.h"
-
 using std::string;
+using std::to_string;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+
+string Format::ElapsedTime(long seconds) {
+  long h = seconds / (60 * 60);
+  long s_remainder = seconds % (60 * 60);
+  long m = s_remainder / 60;
+  long s_final = s_remainder % 60;
+
+  string h_str = string(2 - to_string(h).length(), '0').append(to_string(h));
+  string m_str = string(2 - to_string(m).length(), '0').append(to_string(m));
+  string s_str =
+      string(2 - to_string(s_final).length(), '0').append(to_string(s_final));
+
+  std::cout << h_str << ":" << m_str << ":" << s_str << "\n";
+
+  return h_str + ":" + m_str + ":" + s_str;
+}
